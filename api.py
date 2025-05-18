33fc48a4b96474e6a2777898c72af551879c7daa
Dailin Li <165076853+dylanlidailin@users.noreply.github.com>
2025-05-18 23:10:06 +0800

    Adding more libraries

--- api.py
+++ api.py
@@ -3,6 +3,27 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 # … your langchain imports …
+# load secrets
+from dotenv import load_dotenv
+import os
+
+# core LLM + parsing
+from langchain_openai.chat_models import ChatOpenAI  
+from langchain_core.output_parsers import StrOutputParser
+
+# prompt templating
+from langchain.prompts import PromptTemplate
+
+# document loading & splitting
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# embeddings & vector store
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
+# retrieval + orchestration
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 
 load_dotenv()
 app = FastAPI()
